a7d7ac477f3663d69f82b78ace50862c537584f3
Pavel365 <pavelzhilkov@abv.bg>
2022-09-25 01:05:53 +0300

    Add file

---
+++ RockPaperScissors/RockPaperScissors.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Rock = "Rock";
+            const string Paper = "Paper";
+            const string Scissors = "Scissors";
+
+            Console.Write("Choose [r]ock, [p]aper or [s]cissors: ");
+            string playerMove = Console.ReadLine();
+
+            if (playerMove == "r" || playerMove == "rock")
+            {
+                playerMove = Rock;
+            }
+            else if (playerMove == "p" || playerMove == "paper")
+            {
+                playerMove = Paper;
+            }
+            else if (playerMove == "s" || playerMove == "scissors")
+            {
+                playerMove = Scissors;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input. Try Again ...");
+                return;
+            }
+
+            Random random = new Random();
+            int computerRandomNumber = random.Next(1,4);
+
+            string computerMove = "";
+
+            switch (computerRandomNumber)
+            {
+                case 1:computerMove = Rock;
+                    break;
+                case 2:computerMove = Paper;
+                    break;
+                case 3:computerMove = Scissors;
+                    break;
+            }
+            Console.WriteLine($"The computer chose {computerMove}.");
+
+            if ((playerMove == Rock && computerMove == Scissors) ||
+                (playerMove == Paper && computerMove == Rock) ||
+                (playerMove == Scissors && computerMove == Paper))
+            {
+                Console.WriteLine("You win.");
+            }
+            else if ((playerMove == Rock && computerMove == Paper) ||
+                     (playerMove == Paper && computerMove == Scissors) ||
+                     (playerMove == Scissors && computerMove == Rock))
+            {
+                Console.WriteLine("You lose.");
+            }
+            else
+            {
+                Console.WriteLine("This game was a draw.");
+            }
+        }
+    }
+}
